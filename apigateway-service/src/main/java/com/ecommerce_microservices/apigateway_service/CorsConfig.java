6195270804d1660efa4a9a1275918eb20ea73fa9
Addisalem Wordofa <addaber@gmail.com>
2025-04-14 19:59:25 -0400

    Update CorsConfig.java

--- apigateway-service/src/main/java/com/ecommerce_microservices/apigateway_service/CorsConfig.java
+++ apigateway-service/src/main/java/com/ecommerce_microservices/apigateway_service/CorsConfig.java
@@ -14,6 +14,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOrigin("https://ecommerce-view.vercel.app/");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
 
